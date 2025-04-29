b2d0e675746152f6b23ff6ebaebbc3404d6abb8c
hugovaillant <hgvaillant@gmail.com>
2025-04-29 17:33:52 +0200

    refactor(login): remove unused login screen code and clear token on startup

--- app/src/main/java/fr/harmony/MainActivity.kt
+++ app/src/main/java/fr/harmony/MainActivity.kt
@@ -3,10 +3,6 @@ package fr.harmony
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -20,7 +16,7 @@ import fr.harmony.api.TokenManager
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         val tokenManager = TokenManager(this)
-        tokenManager.clearToken()
+
         var token = tokenManager.getToken()
 
         super.onCreate(savedInstanceState)
